79391b5106f1ccf360f7d3043f83aa933e4f3687
badeshiben <badeshiben@gmail.com>
2022-06-22 14:48:12 -0600

    updated openFAST  outputs

--- BAR_USC/WEIS/Submit.sh
+++ BAR_USC/WEIS/Submit.sh
@@ -3,8 +3,8 @@
 #SBATCH --time 2-00:00:00
 #SBATCH -A bar
 #SBATCH -p standard
-#SBATCH --nodes=6                               # Number of nodes
-#SBATCH --ntasks-per-node=36                    # Number of processors per node
+#SBATCH --nodes=44                           # Number of nodes
+#SBATCH --ntasks-per-node=4                    # Number of processors per node
 #SBATCH --mail-user benjamin.anderson@nrel.gov
 #SBATCH --mail-type BEGIN,END,FAIL
 #SBATCH -o slurm-%x-%j.log                      # Output
@@ -14,4 +14,5 @@ ml conda comp-intel intel-mpi mkl
 module unload gcc
 
 source activate weis-env
-mpirun -np 193 python weis_driver.py
+mpirun -np 175 python weis_driver.py
+
